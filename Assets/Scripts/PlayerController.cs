81d9a75bfd941d00f0e85cdce56b80e80f28c64b
Chien nong <chiennong@Chiens-MacBook-Pro.local>
2024-01-06 17:55:15 +0700

    update player movement, set up tile map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+    Vector2 move;
     public InputAction MoveAction;
     public float speed = 5.0f;
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2D.position + move * speed * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
     }
 }
